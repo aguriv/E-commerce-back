4e601b43189f5deba8bb733272d37e688ff07533
solesienra <solesienra@users.noreply.github.com>
2020-11-11 12:59:38 -0300

    Rutas publicas

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,11 @@ const jwt = require("express-jwt");
 const productController = require("../controllers/productController");
 
 module.exports = function productRoutes(app) {
+  /* RUTAS PUBLICAS */
+  app.get("/api/v1/products", productController.listProducts);
+
   /* RUTAS PRIVADAS */
   app.use(jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
-
-  app.get("/api/v1/products", productController.listProducts);
 
   app.post("/api/v1/products", productController.saveProduct);
 };
