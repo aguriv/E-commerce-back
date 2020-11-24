8e5ff5a9d5f03b0c0c9de06900a462c6faf7b5a4
gonzalomolina1990 <61057264+gonzalomolina1990@users.noreply.github.com>
2020-11-24 15:44:51 -0300

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -11,7 +11,7 @@ module.exports = {
       cart: req.body.cart,
       buyer: req.user.sub,
       totalPrice: req.body.totalPrice,
-      orderState: "no pago",
+      orderState: "No pago",
     });
     order.save();
     order.cart.forEach(async (e) => {
